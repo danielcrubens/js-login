a169b3f6342ae5ac0c3bcc3bb603a5d5ec012a92
danielcrubens <dcr.tecnologi@gmail.com>
2022-07-25 17:34:04 -0300

    autenticação de login :fire:

---
+++ js/login.js
@@ -0,0 +1,65 @@
+
+let btn = document.querySelector('.bi-eye-slash')
+
+btn.addEventListener('click', function () {
+  let inputSenha = document.querySelector('#senha')
+  if (inputSenha.getAttribute('type') == 'password') {
+    inputSenha.setAttribute('type', 'text')
+    this.classList.toggle("bi-eye")
+  } else {
+    inputSenha.setAttribute('type', 'password')
+    this.classList.toggle("bi-eye")
+  }
+})
+
+
+
+function entrar() {
+  let usuario = document.querySelector('#usuario')
+  let userLabel = document.querySelector('#userLabel')
+
+  let senha = document.querySelector('#senha')
+  let senhaLabel = document.querySelector('#senhaLabel')
+
+  let msgError = document.querySelector('#msgError')
+
+  let listaUser = []
+
+  let userValid = {
+    nome: '',
+    user: '',
+    senha: ''
+  }
+  listaUser = JSON.parse(localStorage.getItem('listaUser'))
+
+  /* VARRER A LISTAUSER ITEM POR ITEM */
+  listaUser.forEach((item) => {
+    if (usuario.value == item.userCad && senha.value == item.senhaCad) {
+      userValid = {
+        nome: item.nomeCad,
+        user: item.userCad,
+        senha: item.senhaCad
+      }
+    }
+  })
+  /* TRABALHANDO OBJETOS DE VALIDAÇÃO */
+  
+if (usuario.value == userValid.user && senha.value == userValid.senha) {
+  window.location.href='/inicio.html'
+
+  /* GERAR TOKEN USUÁRIO */
+  let token = Math.round().toString(16).substr(2) + Math.round().toString(16).substr(2)
+  localStorage.setItem('token',token)
+  
+localStorage.setItem('userLogado', JSON.stringify(userValid))
+
+} else {
+  userLabel.setAttribute('style', 'color: red')
+    usuario.setAttribute('style', 'border-color: red')
+    senhaLabel.setAttribute('style', 'color: red')
+    senha.setAttribute('style', 'border-color: red')
+    msgError.setAttribute('style', 'display: block')
+    msgError.innerHTML = 'Usuário ou senha incorretos'
+    usuario.focus()
+}
+}
